86f86754030b91dd4988393e78470a364a9017cb
codemaster9957 <98223070+codemaster9957@users.noreply.github.com>
2022-03-01 07:15:55 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const level_2_player = SpriteKind.create()
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
 	
 })
@@ -6,16 +9,51 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, ot
     music.bigCrash.playUntilDone()
     game.splash("level 1 complete")
     tiles.loadMap(tiles.createMap(tilemap`level6`))
+    mySprite2 = sprites.create(img`
+        ........................
+        ........................
+        ........................
+        ........................
+        ..........ffff..........
+        ........ff1111ff........
+        .......fb111111bf.......
+        .......f11111111f.......
+        ......fd11111111df......
+        ......fd11111111df......
+        ......fddd1111dddf......
+        ......fbdbfddfbdbf......
+        ......fcdcf11fcdcf......
+        .......fb111111bf.......
+        ......fffcdb1bdffff.....
+        ....fc111cbfbfc111cf....
+        ....f1b1b1ffff1b1b1f....
+        ....fbfbffffffbfbfbf....
+        .........ffffff.........
+        ...........fff..........
+        ........................
+        ........................
+        ........................
+        ........................
+        `, SpriteKind.level_2_player)
+    scaling.scaleByPixels(mySprite2, 3, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+    tiles.placeOnTile(mySprite2, tiles.getTileLocation(250, 8))
     tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 10))
     scene.cameraFollowSprite(mySprite)
     mySprite.setVelocity(40, 0)
     music.playMelody("F C A D F E - D ", 120)
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
+    if (controller.A.isPressed()) {
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(225, 3))
+    }
+})
 scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
     game.over(false)
 })
+let mySprite2: Sprite = null
 let mySprite3: Sprite = null
 let mySprite: Sprite = null
+game.showLongText("dodge the blocks and headshot_humans choose a power up", DialogLayout.Bottom)
 mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -34,6 +72,7 @@ mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Projectile)
+tiles.setCurrentTilemap(tilemap`level4`)
 mySprite3 = sprites.create(img`
     . . . . f f f f f f . . . . . . 
     . . . f 2 f e e e e f f . . . . 
@@ -54,14 +93,18 @@ mySprite3 = sprites.create(img`
     `, SpriteKind.Player)
 scaling.scaleByPixels(mySprite3, 20, ScaleDirection.Uniformly, ScaleAnchor.Middle)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 3))
-tiles.setCurrentTilemap(tilemap`level4`)
 scene.cameraFollowSprite(mySprite)
 tiles.placeOnTile(mySprite3, tiles.getTileLocation(250, 4))
 mySprite.setVelocity(30, 0)
-game.onUpdateInterval(2000, function () {
-    mySprite.ax += 5
-    controller.moveSprite(mySprite, 0, 100)
-})
 game.onUpdateInterval(1000, function () {
     info.changeScoreBy(1)
+})
+forever(function () {
+    mySprite.startEffect(effects.fire)
+    mySprite.startEffect(effects.fire)
+    mySprite.startEffect(effects.fire)
+})
+game.onUpdateInterval(10000, function () {
+    mySprite.vx += 10
+    controller.moveSprite(mySprite, 0, 200)
 })
